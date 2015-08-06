817b4ce3535588e544a2c0b84d439fec3dc5e44e
Eduardo Henrique Arnold <eduardoarnoldh@gmail.com>
2015-08-06 14:33:14 -0300

    Implemented 'solve' procedures on server side

--- sudoweb.py
+++ sudoweb.py
@@ -1,16 +1,36 @@
+import os
 import subprocess
+import random
+import string
+import base64
 from flask import Flask, request, render_template
 
-app = Flask(__name__, static_folder='web', template_folder='web', static_url_path='/static')
+##################
+#Helper Functions
+##################
 
+def rnd_str(size=6, chars=string.ascii_uppercase + string.digits):
+        '''Random string generator'''
+        return ''.join(random.choice(chars) for _ in range(size))
+        
 def formatSudokuOutput(lines):
         '''Format Sudosolver output to be displayed in HTML'''
         lines = lines.split('\n')
-        lines.pop(0)
+        
+        if len(lines) < 9:
+                return None      
+                  
+        lines = lines[lines.index('9')+1:]
         lines = [line for line in lines if line != '']
         grid = [x for line in lines for x in line.split(' ') if x!='']
         grid = [x if x!='0' else '' for x in grid]
         return grid
+
+#########
+#Web App
+#########
+
+app = Flask(__name__, static_folder='web', template_folder='web', static_url_path='/static')
 
 @app.route('/')
 def index():
@@ -20,6 +40,61 @@ def index():
 def solve():
         if request.method == 'GET':
                 return render_template("solve.html")
+        else:
+                f = request.files.get('file', None)
+                pic = request.form.get('picture', None)
+                txt = request.form.get('sudotext', None)   
+                
+                err = None
+                
+                #Image file                
+                if f:
+                        #Save file
+                        f = request.files['file']
+                        fname, ext = os.path.splitext(f.filename)
+                        ext = ext.lower()
+                        
+                        if ext in ['.jpg', '.jpeg', '.png']:
+                                path = 'temp/'+rnd_str()+ext
+                                f.save(path)
+                        else:
+                                err = 'Invalid image extension.'                                
+                                
+                #Online picture
+                if pic:
+                        #Save file as png
+                        path = 'temp/'+rnd_str()+'.png'
+                        fl = open(path, 'wb')
+                        pic = pic[pic.find('base64,')+7:]
+                        fl.write(base64.b64decode(pic))
+                        fl.close()
+                                        
+                #Image processing
+                if f or pic:                
+                        #Process image
+                        lines = subprocess.check_output(['./sudosolver.out', '-i', path])
+                        os.remove(path)
+                        grid = formatSudokuOutput(lines)
+                        if grid is None:
+                                err = lines
+                                
+                #Manual text input
+                if txt:
+                        #Write file
+                        path = 'temp/'+rnd_str()+'.txt'
+                        f = open(path, 'w')
+                        f.write('9\n')
+                        f.write(txt)
+                        f.close()
+                        
+                        #Process file
+                        lines = subprocess.check_output(['./sudosolver.out', '-s', path])
+                        os.remove(path)
+                        grid = formatSudokuOutput(lines)
+                        if grid is None:
+                                err = lines                        
+                
+                return render_template("solve.html", grid=grid, err=err)
         
 @app.route('/generate', methods=['GET','POST'])
 def generate():
@@ -41,4 +116,4 @@ def about():
         return render_template("about.html")
 
 if __name__ == '__main__':
-        app.run(threaded=True, debug=True)
+        app.run(host='0.0.0.0',threaded=True, debug=True)
